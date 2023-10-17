d944cbe42315f22e5fe4056eb26ac1de5d6d77ba
atchman <atchman@users.noreply.github.com>
2023-10-17 17:01:49 +0200

    /hypr and /waybar little adjustments for both some style.css adjustments/rewrite

--- .config/hypr/scripts/xdg-portal.sh
+++ .config/hypr/scripts/xdg-portal.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sleep 1
-killall xdg-desktop-portal-hyprland
-killall xdg-desktop-portal-gtk
+killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-gtk
 killall xdg-desktop-portal
 logger 'killed all xdg-desktop'
 sleep 1
 /usr/lib/xdg-desktop-gtk &
 logger 'xdg-desktop-gtk started'
-sleep 1
+sleep 2
 /usr/lib/xdg-desktop-portal-hyprland &
 logger 'xdg-desktop-hyprland started'
 sleep 2

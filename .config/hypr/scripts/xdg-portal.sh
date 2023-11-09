05f68c9c15aea8cab35d18589f9563446818e463
atchman <atchman@users.noreply.github.com>
2023-11-09 17:01:28 +0100

    update /hypr & /i3 & /sway some minor tweaks

--- .config/hypr/scripts/xdg-portal.sh
+++ .config/hypr/scripts/xdg-portal.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-sleep 1
+sleep 1s
 killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-gtk
 killall xdg-desktop-portal
 logger 'killed all xdg-desktop'
-sleep 1
+sleep 1s
 /usr/lib/xdg-desktop-gtk &
 logger 'xdg-desktop-gtk started'
-sleep 2
+sleep 2s
 /usr/lib/xdg-desktop-portal-hyprland &
 logger 'xdg-desktop-hyprland started'
-sleep 2
+sleep 2s
 /usr/lib/xdg-desktop-portal &
 logger 'xdg-desktop-portal started'

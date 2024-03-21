781f7f25f93033ac43242ac0dab868c1b1b8705c
atchman@eos17 <atchman@hotmail.de>
2024-03-21 21:35:19 +0100

    forgot to commit latest changes forced by latest breaking changes in hyprland 0.37 some error still exist, will hopefully be fixed this weekend

--- config/hypr/scripts/xdg-portal.sh
+++ config/hypr/scripts/xdg-portal.sh
@@ -3,13 +3,13 @@ sleep 1s
 killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-gtk
 killall xdg-desktop-portal
-logger 'killed all xdg-desktop'
+logger 'killed all xdg-desktop-portal'
 sleep 1s
-/usr/lib/xdg-desktop-gtk &
-logger 'xdg-desktop-gtk started'
+/usr/lib/xdg-desktop-portal-gtk &
+logger 'xdg-desktop-portal-gtk started'
 sleep 2s
 /usr/lib/xdg-desktop-portal-hyprland &
-logger 'xdg-desktop-hyprland started'
+logger 'xdg-desktop-portal-hyprland started'
 sleep 2s
 /usr/lib/xdg-desktop-portal &
 logger 'xdg-desktop-portal started'

628e4a9503d333abbaf80b49d7cb66180e7cfa57
atchman <atchman@users.noreply.github.com>
2025-04-08 22:49:58 +0200

    feat(config): environment.d environment variables only in one place to find

--- config/fish/conf.d/00-environment.fish
+++ config/fish/conf.d/00-environment.fish
@@ -7,8 +7,8 @@ set -Ux XDG_CACHE_HOME $HOME/.cache
 set -Ux XDG_DATA_HOME $HOME/.local/share
 set -Ux XDG_STATE_HOME $HOME/.local/state
 
-set -gx EDITOR nvim
-set -gx VISUAL nvim
-set -gx MANPAGER 'nvim +Man!'
+# set -Ux EDITOR nvim
+# set -Ux VISUAL nvim
+# set -Ux MANPAGER 'nvim +Man!'
 
 set -Ux LSCOLORS ExGxcxdxCxegDxabagacad

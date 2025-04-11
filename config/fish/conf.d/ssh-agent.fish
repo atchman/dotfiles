ca804253522e41d8ace6dd17e597f2658e434fe5
atchman <atchman@users.noreply.github.com>
2025-04-12 00:41:53 +0200

    fix(fish): ssh-agent
    pls be fixed now

--- config/fish/conf.d/ssh-agent.fish
+++ config/fish/conf.d/ssh-agent.fish
@@ -1,1 +1,11 @@
-eval $(ssh-agent -c)
+# set SSH_AUTH_SOCK if not set
+if test -z "$SSH_AUTH_SOCK";
+  set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket 
+end
+
+# if test -z (pgrep ssh-agent)
+#   eval (ssh-agent -c) > /dev/null
+#   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+#   set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+#   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+# end

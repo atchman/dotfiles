4dc326194ab4586a5f0b125d00529261838a1947
atchman <atchman@users.noreply.github.com>
2026-01-31 12:37:15 +0100

    script: modified startw.sh
    added options to choose between compositor like hyprland and niri

--- script/startw.sh
+++ script/startw.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-# start wayland compositor
-# in terminal multiplexer
+# startx
+# for wayland compositor
 
-# zellij attach --create hypr
+set -euo pipefail
+
+# hyprland
 hypr() {
     zellij -l <(cat <<KDL
 layout {
@@ -16,9 +18,41 @@ KDL
 # tmux
 # tmux new -s hypr Hyprland
 
+# niri
 niri() {
     niri-session
 }
 
+# help
+Help() {
+    echo "start wayland compositor"
+    echo "choose between following options"
+    echo "hypr | h -> Hyprland"
+    echo "niri | n -> niri"
+}
+
 # --- main ---
-niri
+# default: niri
+if [ $# -eq 0 ]; then
+      niri
+fi
+
+OPT=$1
+
+case "$OPT" in
+    "help" | "h")
+        Help
+        exit
+        ;;
+    "hypr")
+        hypr
+        ;;
+    "niri" | "n")
+        niri
+        ;;
+    *)
+        echo "need to choose!!!"
+        Help
+        exit
+        ;;
+esac
